32b9ad43214a29ab0b749d8bc26d1d7e9e95ade7
Mohamed Shanil K K <81458553+shanilmohd@users.noreply.github.com>
2023-06-14 19:09:40 +0530

    Update server.py

--- server.py
+++ server.py
@@ -8,4 +8,4 @@ def req(prompt):
     a = requests.get("https://a7db-35-204-126-85.ngrok-free.app/?prompt="+prompt)
     return(a.text)
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run()
