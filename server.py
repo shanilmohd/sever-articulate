df362d3a15c32cea617a5f47780f5e01ebbaa1f4
Mohamed Shanil K K <81458553+shanilmohd@users.noreply.github.com>
2023-06-15 13:36:02 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_cors import CORS, cross_originfrom flask_cors import CORS, cross_origin
 import requests
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/req/<prompt>")
+@cross_origin()
 def req(prompt):
     a = requests.get("https://53f8-34-123-227-10.ngrok-free.app/?prompt="+prompt)
     return(a.text)
