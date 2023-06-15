5bc22770ff5f2b9bfcaeaf7e073d5dd067b6349a
Mohamed Shanil K K <81458553+shanilmohd@users.noreply.github.com>
2023-06-15 13:27:03 +0530

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/req/<prompt>")
 def req(prompt):
-    a = requests.get("https://090c-34-83-11-209.ngrok-free.app/?prompt="+prompt)
+    a = requests.get("https://53f8-34-123-227-10.ngrok-free.app/?prompt="+prompt)
     return(a.text)
 if __name__=="__main__":
     app.run()
